f61ce314bbbf6fb4d2e167da8d4820099ae7818c
mig <michaelleojacob@gmail.com>
2022-06-26 14:51:25 -0700

    added search

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
-@Component({
+@Component( {
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
-})
+  styleUrls: ['./dashboard.component.css']
+} )
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor ( private heroService: HeroService ) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -18,6 +18,6 @@ export class DashboardComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe( heroes => this.heroes = heroes.slice( 1, 5 ) );
   }
 }
